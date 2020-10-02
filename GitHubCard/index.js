d00fd7e00f5dd92322801639dcfe784344d9c3c1
Andreas Balourdas <dreyuy@outlook.com>
2020-10-01 23:09:32 -0700

    final

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,7 +3,19 @@
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+axios
+  .get("https://api.github.com/users/dreyuy")
+  .then(response => {
+    // deal with the response data in here
 
+    parentCard.append(profileCard(response));
+
+    console.log(response);
+  })
+  .catch(err => {
+    // deal with the error in here
+    console.log("the data was not returned", err);
+  });
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -28,7 +40,6 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -50,6 +61,7 @@ const followersArray = [];
     </div>
 */
 
+
 /*
   List of LS Instructors Github username's:
     tetondan
@@ -58,3 +70,80 @@ const followersArray = [];
     luishrd
     bigknell
 */
+
+
+function profileCard(object) {
+  //creating elements
+  const card = document.createElement("div"),
+    imgUser = document.createElement("img"),
+    cardInfo = document.createElement("div"),
+    userName = document.createElement("h3"),
+    pUser = document.createElement("p"),
+    pLocation = document.createElement("p"),
+    pProfile = document.createElement("p"),
+    aGithub = document.createElement("a"),
+    pFollowers = document.createElement("p"),
+    pFollowing = document.createElement("p"),
+    pBio = document.createElement("p");
+
+  //add class-------------------------------
+  card.classList.add("card");
+  cardInfo.classList.add("card-info");
+  userName.classList.add("name");
+  pUser.classList.add("username");
+
+  //
+  imgUser.src = object.data.avatar_url;
+  pLocation.textContent = `Location: ${object.data.location}`;
+  pProfile.textContent = "Profile: ";
+  aGithub.href = object.data.html_url;
+  // console.log(object.html_url);
+
+  pFollowers.textContent = `Followers: ${object.data.followers}`;
+  pFollowing.textContent = `Following: ${object.data.following}`;
+  pBio.textContent = `Bio: ${object.data.bio}`;
+  aGithub.textContent = object.data.html_url;
+  userName.textContent = object.data.name;
+  pUser.textContent = object.data.login;
+
+  //appending
+
+  card.append(imgUser, cardInfo);
+  cardInfo.append(
+    userName,
+    pUser,
+    pLocation,
+    pProfile,
+    pFollowers,
+    pFollowing,
+    pBio
+  );
+  pProfile.append(aGithub);
+
+  return card;
+}
+
+const parentCard = document.querySelector(".cards");
+
+const followersArray = [
+  "joowoonk",
+  "mtruj013",
+  "imriven",
+  "mrsimpson3000",
+  "devjaymoe"
+];
+
+followersArray
+  .forEach(function(item) {
+    axios
+      .get(`https://api.github.com/users/${item}`)
+      .then(response => {
+        parentCard.append(profileCard(response));
+
+        console.log(response);
+      })
+      .catch(err => {
+        console.log("the data was not returned", err);
+      });
+  })
+// });
